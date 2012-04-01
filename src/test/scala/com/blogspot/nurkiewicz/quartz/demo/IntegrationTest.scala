339377f77501d78cfcc6ad6ee4a73489797a18ad
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-04-01 19:54:52 +0200

    Simple infinitely running job scheduled

--- src/test/scala/com/blogspot/nurkiewicz/quartz/demo/IntegrationTest.scala
+++ src/test/scala/com/blogspot/nurkiewicz/quartz/demo/IntegrationTest.scala
@@ -4,10 +4,16 @@ import org.springframework.test.context.ContextConfiguration
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import javax.annotation.Resource
-import org.quartz.Scheduler
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 import spring.Bootstrap
+import reflect.BeanProperty
+import com.weiglewilczek.slf4s.Logging
+import org.quartz.{Job, JobExecutionContext, Scheduler}
+import java.util.concurrent.TimeUnit
+import org.quartz.SimpleScheduleBuilder._
+import org.quartz.TriggerBuilder._
+import org.quartz.JobBuilder._
 
 /**
  * @author Tomasz Nurkiewicz
@@ -21,6 +27,36 @@ class IntegrationTest extends FunSuite with ShouldMatchers with SpringRule {
 	val scheduler: Scheduler = null
 
 	test("Bootstrap test") {
+		val trigger = newTrigger().
+				withIdentity("Every-few-seconds", "Demo").
+				withSchedule(
+			simpleSchedule().
+					withIntervalInSeconds(4).
+					repeatForever()
+		).
+				build()
+
+		val job = newJob(classOf[PrintMessageJob]).
+				withIdentity("Print-message", "Demo").
+				usingJobData("msg", "Hello, world!").
+				build()
+
+		scheduler.scheduleJob(job, trigger)
+
+		TimeUnit.MINUTES.sleep(10)
 	}
 
+}
+
+class PrintMessageJob extends Job with Logging {
+
+	@BeanProperty
+	var printer: Printer = _
+
+	@BeanProperty
+	var msg = ""
+
+	def execute(context: JobExecutionContext) {
+		printer print msg
+	}
 }
